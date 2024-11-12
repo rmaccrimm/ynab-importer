7fd53d6e7200501d2f71b739557cf08441b2978c
Roddy MacCrimmon <r.maccrimm@gmail.com>
2024-11-11 21:15:05 -0700

    add script for generating api client

---
+++ generate_client.ps1
@@ -0,0 +1,5 @@
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate `
+    -i /local/ynab_spec.yaml `
+    -g rust  `
+    -o /local/api-lib `
+    --additional-properties=packageName=ynab_api
