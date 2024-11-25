94026f4d86c047706dd5705d555137c16971d3bc
Roddy MacCrimmon <r.maccrimm@gmail.com>
2024-11-24 19:58:11 -0700

    Initial implmentation for OFX file parser

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -1,20 +1,56 @@
-use notify_debouncer_full::{new_debouncer, notify::*, DebounceEventResult, DebouncedEvent};
-use std::{sync::mpsc, time::Duration};
+use notify_debouncer_full::notify::{
+    event::{CreateKind, Event},
+    EventKind::{Access, Create, Modify, Other, Remove},
+    RecursiveMode,
+};
+use notify_debouncer_full::{new_debouncer, DebounceEventResult, DebouncedEvent};
+use rusqlite::Connection;
+use std::{error::Error, fs};
+use std::{
+    sync::mpsc::{channel, Receiver},
+    time::Duration,
+};
+use ynab_importer::db::{account, budget, config};
 
-fn main() -> Result<()> {
-    let (tx, rx) = mpsc::channel();
+fn create_transactions(conn: &Connection, event: &Event) {
+    if event.paths.len() == 0 {
+        println!("No paths provided with create event.");
+        return;
+    }
+    match fs::read_to_string(&event.paths[0]) {
+        Ok(content) => {
+            todo!();
+            // let qif = parse(&content, "%d/%m/%Y").expect("Failed to parse");
+            // println!("{:#?}", qif);
+        }
+        Err(_) => println!("Failed to read file"),
+    }
+}
 
-    // Select recommended watcher for debouncer.
-    // Using a callback here, could also be a channel.
+// fn get_budget_and_account_for_path
+fn event_handler(conn: &Connection, event: &DebouncedEvent) {
+    match event.kind {
+        Create(CreateKind::File) | Create(CreateKind::Any) => create_transactions(&conn, &event),
+        _ => println!("Ignored event {:#?}", event),
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let conn = Connection::open("./db.sqlite3")?;
+
+    let (tx, rx) = channel();
     let mut debouncer = new_debouncer(Duration::from_secs(2), None, tx)?;
 
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    debouncer.watch("./downloads", RecursiveMode::Recursive)?;
+    let watch_dir = config::get(&conn, config::TRANSACTION_DIR)?;
+    debouncer.watch(&watch_dir, RecursiveMode::Recursive)?;
 
     for res in rx {
         match res {
-            Ok(event) => println!("event: {:?}", event),
+            Ok(events) => {
+                for event in events.iter() {
+                    event_handler(&conn, event);
+                }
+            }
             Err(e) => println!("watch error: {:?}", e),
         }
     }
---
+++ src/ofx.rs
@@ -0,0 +1,174 @@
+use chrono;
+use chrono::{serde::ts_milliseconds, DateTime, NaiveDateTime, Utc};
+use regex::Regex;
+use serde::{de, Deserialize, Deserializer};
+use sgmlish;
+
+#[derive(Debug, Deserialize)]
+struct Ofx {
+    #[serde(rename = "BANKMSGSRSV1")]
+    response: BankMessagesResponseV1,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BankMessagesResponseV1 {
+    #[serde(rename = "STMTTRNRS")]
+    response: StatementTransactionResponse,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct StatementTransactionResponse {
+    #[serde(rename = "STMTRS")]
+    response: StatementResponse,
+}
+
+#[derive(Debug, Deserialize)]
+struct StatementResponse {
+    #[serde(rename = "BANKTRANLIST")]
+    transaction_list: TransactionList,
+}
+
+#[derive(Debug, Deserialize)]
+struct TransactionList {
+    #[serde(rename = "STMTTRN")]
+    transactions: Vec<OfxTransaction>,
+}
+
+fn deserialize_datetime<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct YMDStringVisitor;
+
+    impl<'de> de::Visitor<'de> for YMDStringVisitor {
+        type Value = NaiveDateTime;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("a datetime string in the format %Y%m%d%H%M%S%.3f")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            NaiveDateTime::parse_from_str(v, "%Y%m%d%H%M%S%.3f")
+                .map_err(|_| E::custom(format!("Failed to parse datetime: {}", v)))
+        }
+    }
+
+    deserializer.deserialize_str(YMDStringVisitor)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct OfxTransaction {
+    #[serde(rename = "TRNTYPE")]
+    pub transaction_type: String,
+
+    #[serde(rename = "DTPOSTED", deserialize_with = "deserialize_datetime")]
+    pub date_posted: NaiveDateTime,
+
+    #[serde(rename = "TRNAMT")]
+    pub amount: f64,
+
+    #[serde(rename = "NAME")]
+    pub name: Option<String>,
+
+    #[serde(rename = "MEMO")]
+    pub memo: Option<String>,
+}
+
+// Just assume that the XML portion extends to the end of the file
+fn get_ofx_block(file_contents: &str) -> Option<&str> {
+    let re = Regex::new("<OFX>").unwrap();
+    let m = re.find(file_contents)?;
+    Some(&file_contents[m.start()..])
+}
+
+pub fn parse(file_contents: &str) -> Result<Vec<OfxTransaction>, sgmlish::Error> {
+    let xml = get_ofx_block(file_contents).unwrap();
+    let sgml = sgmlish::Parser::builder().parse(xml)?;
+    let sgml = sgmlish::transforms::normalize_end_tags(sgml)?;
+    let result = sgmlish::from_fragment::<Ofx>(sgml)?;
+    Ok(result
+        .response
+        .response
+        .response
+        .transaction_list
+        .transactions)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::NaiveDate;
+
+    const SAMPLE: &str = "\
+        OFXHEADER:100\
+        DATA:OFXSGML\
+        VERSION:102\
+        SECURITY:NONE\
+        ENCODING:USASCII\
+        CHARSET:1252\
+        COMPRESSION:NONE\
+        OLDFILEUID:NONE\
+        NEWFILEUID:NONE\
+        \
+        <OFX><SIGNONMSGSRSV1><SONRS><STATUS><CODE>0<SEVERITY>INFO<MESSAGE>Authentication \
+        Successful.</STATUS><DTSERVER>20241120170806.513[-5:EST]<LANGUAGE>ENG<FI><ORG>Tangerine\
+        <FID>12345</FI><INTU.BID>12345</SONRS></SIGNONMSGSRSV1><BANKMSGSRSV1><STMTTRNRS><TRNUID>0\
+        <STATUS><CODE>0<SEVERITY>INFO</STATUS><STMTRS><CURDEF>CAD<BANKACCTFROM><BANKID>1234<ACCTID>\
+        1111111111111111<ACCTTYPE>CREDITLINE</BANKACCTFROM><BANKTRANLIST><DTSTART>\
+        20241102200000.000[-4:EDT]<DTEND>20241120190000.000[-5:EST]
+        <STMTTRN>\
+            <TRNTYPE>DEBIT\
+            <DTPOSTED>20241117120000.000\
+            <TRNAMT>-0.5\
+            <FITID>0000000000001\
+            <NAME>PARKING PAY MACHINE\
+        </STMTTRN>\
+        <STMTTRN>\
+            <TRNTYPE>DEBIT\
+            <DTPOSTED>20241117120000.000\
+            <TRNAMT>-7.88\
+            <FITID>0000000000002\
+            <NAME>SQ ICECREAM\
+            <MEMO>Rewards earned: 0.04 ~ Category: Other\
+        </STMTTRN>\
+        <STMTTRN>\
+            <TRNTYPE>DEBIT\
+            <DTPOSTED>20241117120000.000\
+            <TRNAMT>-7.35\
+            <FITID>0000000000003\
+            <NAME>PIZZA RESTAURANT\
+            <MEMO>Rewards earned: 0.04 ~ Category: Restaurant\
+        </STMTTRN>\
+        <STMTTRN>\
+            <TRNTYPE>DEBIT\
+            <DTPOSTED>20241112120000.000\
+            <TRNAMT>-8.91\
+            <FITID>0000000000004\
+            <NAME>City Mall\
+            <MEMO>Rewards earned: 0.18 ~ Category: Entertainment\
+        </STMTTRN>\
+        </BANKTRANLIST><LEDGERBAL><BALAMT>-276.39<DTASOF>20241120170806.513[-5:EST]</LEDGERBAL>\
+        <AVAILBAL><BALAMT>-11692.05<DTASOF>20241120170806.513[-5:EST]</AVAILBAL></STMTRS>\
+        </STMTTRNRS></BANKMSGSRSV1></OFX>\
+        ";
+
+    #[test]
+    fn test_parse() {
+        let transactions = parse(&SAMPLE).unwrap();
+        println!("{transactions:#?}");
+    }
+
+    #[test]
+    fn test_chrono_deserialize() {
+        assert_eq!(
+            NaiveDateTime::parse_from_str("20241117120000.000", "%Y%m%d%H%M%S%.3f").unwrap(),
+            NaiveDate::from_ymd_opt(2024, 11, 17)
+                .unwrap()
+                .and_hms_milli_opt(12, 0, 0, 0)
+                .unwrap()
+        );
+    }
+}
