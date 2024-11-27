01289a3c0b02bae6b823d362f81f0cda7c97f1ec
Roddy MacCrimmon <r.maccrimm@gmail.com>
2024-11-26 21:48:14 -0700

    Work on file imports

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -5,23 +5,78 @@ use notify_debouncer_full::notify::{
 };
 use notify_debouncer_full::{new_debouncer, DebouncedEvent};
 use rusqlite::Connection;
-use std::{error::Error, fs};
+use std::{error::Error, fs, path::PathBuf};
 use std::{sync::mpsc::channel, time::Duration};
-use ynab_importer::db::config;
+use uuid::Uuid;
+use ynab_api::models::{
+    NewTransaction, TransactionClearedStatus, TransactionDetail, TransactionResponseData,
+};
 use ynab_importer::ofx::parse;
+use ynab_importer::{db::config, ofx::OfxTransaction};
 
-fn create_transactions(conn: &Connection, event: &Event) {
+fn load_transactions(path: &PathBuf) -> Vec<OfxTransaction> {
+    let path = &event.paths[0];
+    match fs::read_to_string(path) {
+        Ok(content) => match parse(&content) {
+            Ok(transactions) => {
+                return transactions;
+            }
+            Err(err) => {
+                println!(
+                    "Failed to parse file {}: {}",
+                    &path.display(),
+                    err.to_string()
+                )
+            }
+        },
+        Err(err) => {
+            println!(
+                "Failed to read file {}: {}",
+                path.display(),
+                err.to_string()
+            );
+        }
+    }
+    vec![]
+}
+
+fn get_account_id_from_path(conn: &Connection, path: &PathBuf) -> Option<UUId> {
+    todo!();
+}
+
+fn create_transactions(conn: &Connection, event: &DebouncedEvent) {
     if event.paths.len() == 0 {
         println!("No paths provided with create event.");
         return;
     }
-    match fs::read_to_string(&event.paths[0]) {
-        Ok(content) => {
-            let transactions = parse(&content).unwrap();
-            println!("{transactions:#?}");
+    let path = &event.paths[0];
+
+    let account_id = match get_account_id_from_path(conn, path) {
+        Some(id) => id,
+        None => {
+            println!(
+                "Import failed. Could not determine account from path {}",
+                path.display()
+            );
+            return;
         }
-        Err(_) => println!("Failed to read file"),
-    }
+    };
+
+    let new_transactions = load_transactions(path).iter().map(|t| NewTransaction {
+        account_id: account_id,
+        date: Some(t.date_posted.to_string()),
+        amount: Some(t.amount),
+        payee_id: None,
+        payee_name: Some(t.name),
+        category_id: None,
+        memo: Some(t.memo),
+        cleared: Some(TransactionClearedStatus::Cleared),
+        approved: None,
+        flag_color: None,
+        subtransactions: None,
+        import_id: None,
+    });
+    todo!();
 }
 
 // fn get_budget_and_account_for_path
