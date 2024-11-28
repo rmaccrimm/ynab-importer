1697d500bf9b6b2095b8b4808fc132b3590c969d
Roddy MacCrimmon <r.maccrimm@gmail.com>
2024-11-27 20:49:30 -0700

    Define ImportError enum, work on logic to get budget from path

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -5,8 +5,15 @@ use notify_debouncer_full::notify::{
 };
 use notify_debouncer_full::{new_debouncer, DebouncedEvent};
 use rusqlite::Connection;
-use std::{error::Error, fs, path::PathBuf};
+use std::{
+    error::Error,
+    ffi::OsString,
+    fs, io,
+    path::{Path, PathBuf},
+};
+use std::{ffi::OsStr, fmt::Write};
 use std::{sync::mpsc::channel, time::Duration};
+use thiserror::Error;
 use uuid::Uuid;
 use ynab_api::models::{
     NewTransaction, TransactionClearedStatus, TransactionDetail, TransactionResponseData,
@@ -14,8 +21,28 @@ use ynab_api::models::{
 use ynab_importer::ofx::parse;
 use ynab_importer::{db::config, ofx::OfxTransaction};
 
+#[derive(Error, Debug)]
+pub enum ImportError {
+    #[error("something went wrong parsing the event path '{0}'")]
+    PathParsingError(String),
+
+    #[error("failed to parse QFX file")]
+    FileParsingError(#[from] sgmlish::Error),
+
+    #[error("no paths provided with event")]
+    NoPathError,
+
+    #[error("missing configuration {0}")]
+    MissingConfigurationError(String),
+
+    #[error("something went wrong talking to the database")]
+    DBError(#[from] rusqlite::Error),
+
+    #[error("failed to load file")]
+    FileIOError(#[from] io::Error),
+}
+
 fn load_transactions(path: &PathBuf) -> Vec<OfxTransaction> {
-    let path = &event.paths[0];
     match fs::read_to_string(path) {
         Ok(content) => match parse(&content) {
             Ok(transactions) => {
@@ -40,32 +67,66 @@ fn load_transactions(path: &PathBuf) -> Vec<OfxTransaction> {
     vec![]
 }
 
-fn get_account_id_from_path(conn: &Connection, path: &PathBuf) -> Option<UUId> {
-    todo!();
+fn get_budget_name_from_path(
+    basedir_path: &PathBuf,
+    path: &PathBuf,
+) -> Result<OsString, ImportError> {
+    let mut display_path = String::new();
+    write!(&mut display_path, "{}", path.display()).unwrap();
+
+    let mut display_basedir = String::new();
+    write!(&mut display_basedir, "{}", path.display()).unwrap();
+
+    let mut path = path.clone();
+    while path.parent() != None {
+        let parent = match path.parent() {
+            Some(p) => p,
+            None => {
+                break;
+            }
+        };
+        if parent == basedir_path {
+            match path.file_name() {
+                Some(p) => {
+                    return Ok(p.to_owned());
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+        if !path.pop() {
+            break;
+        }
+    }
+    Err(ImportError::PathParsingError(display_path))
 }
 
-fn create_transactions(conn: &Connection, event: &DebouncedEvent) {
+fn create_transactions(conn: &Connection, event: &DebouncedEvent) -> Result<(), ImportError> {
     if event.paths.len() == 0 {
-        println!("No paths provided with create event.");
-        return;
+        return Err(ImportError::NoPathError);
     }
     let path = &event.paths[0];
 
-    let account_id = match get_account_id_from_path(conn, path) {
-        Some(id) => id,
-        None => {
-            println!(
-                "Import failed. Could not determine account from path {}",
-                path.display()
-            );
-            return;
-        }
-    };
+    let base_dir = match config::get(&conn, config::TRANSACTION_DIR) {
+        Ok(dir) => Ok(dir),
+        Err(rusqlite::Error::QueryReturnedNoRows) => Err(ImportError::MissingConfigurationError(
+            config::TRANSACTION_DIR.into(),
+        )),
+        Err(err) => Err(err.into()),
+    }?;
+    let base_dir = PathBuf::from(base_dir).canonicalize()?;
+    println!("{:?}", base_dir);
+
+    let account_id = get_budget_name_from_path(&base_dir, path)?;
+    println!("{:?}", account_id);
+
+    return Ok(());
 
     let new_transactions = load_transactions(path).iter().map(|t| NewTransaction {
-        account_id: account_id,
+        account_id: todo!(),
         date: Some(t.date_posted.to_string()),
-        amount: Some(t.amount),
+        amount: todo!(),
         payee_id: None,
         payee_name: Some(t.name),
         category_id: None,
@@ -80,10 +141,13 @@ fn create_transactions(conn: &Connection, event: &DebouncedEvent) {
 }
 
 // fn get_budget_and_account_for_path
-fn event_handler(conn: &Connection, event: &DebouncedEvent) {
+fn event_handler(conn: &Connection, event: &DebouncedEvent) -> Result<(), ImportError> {
     match event.kind {
         Create(CreateKind::File) | Create(CreateKind::Any) => create_transactions(&conn, &event),
-        _ => println!("Ignored event {:#?}", event),
+        _ => {
+            println!("Ignored event {:#?}", event);
+            Ok(())
+        }
     }
 }
 
@@ -100,11 +164,45 @@ fn main() -> Result<(), Box<dyn Error>> {
         match res {
             Ok(events) => {
                 for event in events.iter() {
-                    event_handler(&conn, event);
+                    match event_handler(&conn, event) {
+                        Err(err) => {
+                            println!("{}", err.to_string());
+                        }
+                        _ => (),
+                    };
                 }
             }
             Err(e) => println!("watch error: {:?}", e),
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_path_ops() {
+        let mut path = PathBuf::from(r"C:\Users\Roddy\Repos\ynab_importer\downloads\AppTestingBudget\Test MasterCard\madeup.QFX")
+            .canonicalize()
+            .unwrap();
+        println!("{:?}", path);
+        let download_dir = PathBuf::from(r"\\?\C:\Users\Roddy\Repos\ynab_importer\downloads");
+        let base_dir = download_dir.file_name().unwrap();
+
+        let mut subdir = None;
+        while path.parent() != None {
+            let parent = path.parent().unwrap().file_name().unwrap();
+            println!("{:?}", parent);
+            if parent == base_dir {
+                subdir = Some(path.file_name());
+                break;
+            }
+            if !path.pop() {
+                break;
+            }
+        }
+        println!("{:?}", subdir);
+    }
 }
