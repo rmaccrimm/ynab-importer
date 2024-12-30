b5356638d76b536e0f5e9a18d24c165619faee5c
Roddy MacCrimmon <r.maccrimm@gmail.com>
2024-12-30 12:08:08 -0700

    Switch to using tokio::main

--- src/bin/configure_ui.rs
+++ src/bin/configure_ui.rs
@@ -1,36 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
-use std::time::Duration;
-use tokio::runtime::Runtime;
 use ynab_importer::ui::ConfigApp;
 
 #[tokio::main]
 async fn main() -> eframe::Result {
-    let rt = Runtime::new().expect("Unable to create Runtime");
-
-    // Enter the runtime so that `tokio::spawn` is available immediately.
-    let _enter = rt.enter();
-
-    // Execute the runtime in its own thread.
-    // The future doesn't have to do anything. In this example, it just sleeps forever.
-    std::thread::spawn(move || {
-        rt.block_on(async {
-            loop {
-                tokio::time::sleep(Duration::from_secs(3600)).await;
-            }
-        })
-    });
-
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([640.0, 240.0]) // wide enough for the drag-drop overlay text
+            .with_inner_size([640.0, 280.0])
             .with_drag_and_drop(true),
         ..Default::default()
     };
     eframe::run_native(
-        "Native file dialogs and drag-and-drop files",
+        "YNAB Importer",
         options,
-        Box::new(|_cc| Ok(Box::new(ConfigApp::new()))),
+        Box::new(|cc| Ok(Box::new(ConfigApp::new(cc)))),
     )
 }
